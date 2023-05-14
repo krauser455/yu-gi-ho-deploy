6de2b04a186a7e6d81081c7bb80b0f64cebe75ca
Andrea <andrea.franco96@virgilo.it>
2023-05-14 23:27:11 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
-const environment = process.env.NODE_ENV === "production"? "production": "environment";
+const environment = process.env.NODE_ENV === "production"? "production": "development";
 const config = require('./config.json')[environment];
 //console.log(config);
 
 console.log( "DIRECTORY PRINCIPALE: " + process.env.PWD)
 
-require('/app/app.js').start( config.dbfile, config.port);
+require('./apps/app').start( config.dbfile, config.port);
