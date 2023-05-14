28c4188083acebf1254f53b4a4a488da67c30526
Andrea <andrea.franco96@virgilo.it>
2023-05-14 18:47:34 +0200

    aaaa

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const environment = process.env.NODE_ENV === "production"? "production": "produc
 const config = require('./config.json')[environment];
 //console.log(config);
 
-console.log(process.env.NODE_ENV)
-console.log("prova")
+console.log( "DIRECTORY PRINCIPALE: " + process.env.PWD)
 
 require('./app/app.js').start( config.dbfile, config.port);
